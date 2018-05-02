aeb663f46506edaf3b0b783257acfd29a10fe507
djcuze <natejacobymusic@gmail.com>
2018-05-02 13:56:13 +1000

    added vue

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Books</title>
+    <script src="app/assets/vendor/vue.js" defer></script>
+    <script src="app/assets/vendor/vue-router.js" defer></script>
+    <script src="app/assets/vendor/jquery-3.3.1.min.js"></script>
+    <script src="app/assets/vendor/axios.js" defer></script>
 </head>
 <body>
 <?php include_once 'app/views/layout/header.php' ?>
-<main class="content">
+<main class="content">g
     <article class="book">
         <h1>Book Title</h1>
         <img src="http://placehold.it/114/162" alt="A Book Cover" class="book__cover"/>
